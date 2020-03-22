b4ce0876376585912529c9003a04f880460e601d
jinwoojung <jwjung5038@gmail.com>
2020-03-22 19:50:00 +0900

    Add test 어노테이션

--- src/test/java/me/jung/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/jung/demoinflearnrestapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.jung.demoinflearnrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.jung.demoinflearnrestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -54,6 +55,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -86,6 +88,7 @@ public class EventControllerTests {
 //                .andExpect(header().(HttpHeaders.CONTENT_TYPE, HAL_JSON.toString()));
     }
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_badRequest() throws Exception {
         Event event = Event.builder()
                 .id(100)// X
@@ -114,6 +117,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생")
     public void createEvent_Bad_Request_Empty_Input () throws Exception {
         EventDto  eventDto = EventDto.builder().build();
         mockMvc.perform(post("/api/events")
@@ -123,6 +127,7 @@ public class EventControllerTests {
                 .andExpect(status().isBadRequest());
     }
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생")
     public void createEvent_Bad_Request_Wrong_Input () throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
